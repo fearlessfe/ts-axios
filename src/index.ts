5e32917a10ae942d5cb6176668a676d72742654e
fearlessfe <505380967@qq.com>
2019-11-15 15:58:04 +0800

    feat:处理body内参数

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { buildURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
 
 function axios (config: AxiosRequestConfig) {
   // todo
+  processConfig(config)
   xhr(config)
+}
+
+function processConfig(config: AxiosRequestConfig) : void {
+  config.url = transformURL(config)   // 处理params
+  config.data = transformRequestData(config)   // 处理data
+}
+
+function transformURL(config: AxiosRequestConfig) : string {
+  return buildURL(config.url, config.params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
 }
 
 export default axios;
